0b66a372124a5bdd853330456b641af95b1d4d57
girtonman <girtonman@gmail.com>
2019-06-30 13:21:20 -0500

    Add Tiler tileset files

---
+++ RoboRally/Boards/Pits.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="Pits" tilewidth="300" tileheight="300" tilecount="28" columns="7">
+ <image source="../ClientApp/src/assets/tilesets/2005TilesetPits.png" trans="ff00ff" width="2100" height="1200"/>
+</tileset>
